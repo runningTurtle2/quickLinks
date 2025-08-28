2f86d9db2cdc592af6b8fb2b4cd2e9b38c08f214
runningTurtle2 <oscarmoren2010@gmail.com>
2025-08-28 16:00:31 -0700

    image branch

--- scripts/saveFunction.js
+++
@@ -1,6 +0,0 @@
-document.getElementById("btn-save").addEventListener("click", () => {
-  const platform = document.getElementById("plug").value;
-  const url = document.getElementById("link").value;
-  
-  localStorage.setItem(platform, url)
-});
